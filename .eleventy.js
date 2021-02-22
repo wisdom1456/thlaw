80672634387df1ad874a9adbae0ffbdf019414f7
wisdom1456 <Modible@gmail.com>
2021-02-22 16:19:02 -0600

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
+
+const CONFIG = {
+  dir: {
+    input: "src",
+    output: "_site",
+    include: "includes",
+  },
+  passthroughsFileCopy: true,
+  dataTemplateEngine: "njk",
+  markdownTemplateEngine: "njk",
+  templateFormats: ["html", "njk", "md"],
+};
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(lazyImagesPlugin, {
+    imgSelector: '.lazyimages img',
+  });
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return CONFIG;
+};
